13e8b262adbc0fed60ed5ccad2f41b62658720f4
ducanh2k5hust-sudo <ducanh2k5hust@gmail.com>
2025-12-24 18:45:05 +0700

    Update luu-diem.php

--- luu-diem.php
+++ luu-diem.php
@@ -6,11 +6,11 @@ if (isset($_SESSION['user_id']) && isset($_POST['score'])) {
     $user_id = $_SESSION['user_id'];
     $score = intval($_POST['score']);
     
-    // Nhận tên game từ trò chơi gửi lên (Nếu không có thì mặc định là Game đếm số)
+    
     $game_name = isset($_POST['game_name']) ? $_POST['game_name'] : 'Đếm hình đoán số';
 
     $stmt = $conn->prepare("INSERT INTO scores (user_id, score, game_name) VALUES (?, ?, ?)");
-    // Sửa chỗ này: "iis" nghĩa là (Số nguyên, Số nguyên, Chuỗi chữ)
+
     $stmt->bind_param("iis", $user_id, $score, $game_name);
     
     if ($stmt->execute()) {
