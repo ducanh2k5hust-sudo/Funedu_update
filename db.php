3e0f2cc3f6ee01faaba179bd444a07ec49420337
ducanh2k5hust-sudo <ducanh2k5hust@gmail.com>
2025-12-24 18:44:02 +0700

    Update db.php

--- db.php
+++ db.php
@@ -3,13 +3,10 @@ $servername = "sql103.infinityfree.com";
 $username = "if0_40752677";
 $password = "3825216a";
 $dbname = "if0_40752677_five_edu";
-
-// Tạo kết nối
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-// Kiểm tra kết nối
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
 }
 $conn->set_charset("utf8");
+
 ?>
